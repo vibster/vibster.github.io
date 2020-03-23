24ad5f1eeb517d23a679aea37ed08473b3c4cbfb
Gautam Mittal <gautam@mittal.net>
2020-03-22 20:26:18 -0700

    Add images, better layout, click eventListener.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,28 @@
 let grid = document.getElementsByClassName('grid')[0];
 
+function register(event) {
+  let objectHierarchy = event.path;
+  for (var obj of objectHierarchy) {
+    if (obj.tagName == 'ARTICLE') {
+      console.log(obj.id);
+      break;
+    }
+  }
+}
+
 fetch('data.json')
   .then((response) => {
-      return response.json();
+    return response.json();
   })
   .then((data) => {
     for (var i = 0; i < data.articles.length; i += 1) {
-        let panel = document.createElement('article');
-        panel.className = 'article';
-        panel.innerHTML = `<h4>${data.articles[i].title}</h4><p>${data.articles[i].description}</p>`;
-        let startTrack = Math.floor(Math.random() * 4) + 1;
-        let endTrack = Math.floor(Math.random() * 4) + 1;
-        panel.style = `grid-column: ${startTrack} / ${endTrack};`;
-        grid.appendChild(panel);
+      let panel = document.createElement('article');
+      panel.id = i;
+      panel.className = 'article';
+      panel.innerHTML = `<h4><a href="${data.articles[i].url}">${data.articles[i].title}</a></h4>` + (data.articles[i].urlToImage != null ? `<img src="${data.articles[i].urlToImage}" />` : ``) + `<p>${data.articles[i].description}</p>`;
+      let priority = Math.floor(Math.random() * 4) + 1;
+      panel.style = `grid-column: span ${priority};`;
+      panel.addEventListener("click", register);
+      grid.appendChild(panel);
     }
   });
