dfb258e6e2de29f72ff91ffb8889eea72288aa9d
Gautam Mittal <gautam@mittal.net>
2020-03-22 17:39:14 -0700

    Add data source.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
-let grid = document.getElementsByClassName('grid')[0]
+let grid = document.getElementsByClassName('grid')[0];
 
-for (var i = 0; i < 40; i += 1) {
-    let panel = document.createElement('article')
-    panel.className = 'article'
-    panel.innerText = i
-
-    // panel.style = 'grid-column: -3 / -1;'
-    grid.appendChild(panel)
-}
+fetch('data.json')
+  .then((response) => {
+      return response.json();
+  })
+  .then((data) => {
+    for (var i = 0; i < data.articles.length; i += 1) {
+        let panel = document.createElement('article');
+        panel.className = 'article';
+        panel.innerHTML = `<h4>${data.articles[i].title}</h4><p>${data.articles[i].description}</p>`;
+        let startTrack = Math.floor(Math.random() * 4) + 1;
+        let endTrack = Math.floor(Math.random() * 4) + 1;
+        panel.style = `grid-column: ${startTrack} / ${endTrack};`;
+        grid.appendChild(panel);
+    }
+  });
